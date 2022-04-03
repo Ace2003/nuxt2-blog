dd54cd00569791a1b5ccb054c69feeeee55672a8
ZengChao <50036071+Ace2003@users.noreply.github.com>
2022-04-03 21:00:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export default {
   githubName: 'Ace2003', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
-  domain: 'https://ace2003.github.io', // rss域名
+  domain: '', // rss域名
   SEO_description: 'Ace的博客，书写值得书写的东西',
   SEO_keywords: 'Ace2003,Ace\'s blog,Ace,Ace的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc" // cloudflare的webanalyze
